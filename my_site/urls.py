6f40d351b323982e1a4a948f575dc54a1b254543
R3tr044 <rcorrero44@proton.me>
2026-05-26 23:40:09 +0200

    Fix imatges en local i actualització README

--- my_site/urls.py
+++ my_site/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
-from django.views.static import serve
 from django.urls import path, include
-from django.conf import settings               # <-- Añade esta importación
-from django.conf.urls.static import static     # <-- Añade esta importación
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
-    path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
-] 
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),  
+]
 
-# Esto le dice a Django que sirva los archivos de la carpeta media en desarrollo
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
+]
